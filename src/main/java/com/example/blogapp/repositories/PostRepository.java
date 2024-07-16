99ee4a8b17de59263b448d0f650806687974f242
pilakaravi.rs143@gmail.com <pilakaravi.rs143@gmail.com>
2024-07-16 08:19:58 +0530

    Added MongoRepository for mongoDB connection

--- src/main/java/com/example/blogapp/repositories/PostRepository.java
+++ src/main/java/com/example/blogapp/repositories/PostRepository.java
@@ -1,7 +1,9 @@
 package com.example.blogapp.repositories;
 
-import com.example.blogapp.models.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PostRepository extends JpaRepository<Post, Integer> {
+import com.example.blogapp.models.Post;
+// import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends MongoRepository<Post, String> {
 }
