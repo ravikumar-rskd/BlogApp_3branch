ac9e34380b8f69e442bfee241dbc71e965968fbd
pilakaravi.rs143@gmail.com <pilakaravi.rs143@gmail.com>
2024-07-16 08:18:12 +0530

    crossorigin for userController with import library

--- src/main/java/com/example/blogapp/controllers/UserController.java
+++ src/main/java/com/example/blogapp/controllers/UserController.java
@@ -1,11 +1,13 @@
 package com.example.blogapp.controllers;
-
+import java.util.List;
 import com.example.blogapp.models.User;
 import com.example.blogapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("/api/users")
 public class UserController {
     @Autowired
@@ -14,6 +16,11 @@ public class UserController {
     @PostMapping
     public User createUser(@RequestBody User user) {
         return userService.createUser(user);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users=userService.getAllUsers();
+            return ResponseEntity.ok(users);
     }
 
     // Other endpoints for user management
